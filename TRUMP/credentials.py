a294cbace52ff6e2b3ba51f786fe870447d84256
Marta Alonso <marta.alonso.fernandez@gmail.com>
2018-05-29 13:39:21 +0200

    Added TRUMP folder with starting code for analyzing Trump tweets.

---
+++ TRUMP/credentials.py
@@ -0,0 +1,12 @@
+# Credentials for your Twitter bot account
+
+# 1. Sign into Twitter or create new account
+# 2. Make sure your mobile number is listed at twitter.com/settings/devices
+# 3. Head to apps.twitter.com and select Keys and Access Tokens
+
+CONSUMER_KEY = 'FATaffM0Hqr8tEKu6BpPoO5lp'
+CONSUMER_SECRET = 'e05JuDF4t8MOtur5yuLqMpIJswAsK3vusmADCX8ep7rmJvjXTk'
+
+# Create a new Access Token
+ACCESS_TOKEN = '781575163016056832-TW9afoGMfAUezKDgddOz22ALqL5inmC' 
+ACCESS_SECRET = '4PJ3JtH6sMJwAo7mmtDebzbLjwESGengCRW4l5P9HtQw8'
